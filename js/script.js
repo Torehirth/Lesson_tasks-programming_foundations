aa2539e243203798e4a29e41fd69cb7c7565bbeb
Tore Mæland Hirth <torehirth@gmail.com>
2023-09-30 11:05:14 +0200

    add defer to script head html, add question 1 lesson task module 1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 console.log("Lesson tasks");
 
+//Question 1
+var number1 = parseInt(6);
+var number2 = parseFloat(4.5);
+var totalnumber = number1 + number2;
+console.log(totalnumber);
+
+// Question 2
+
+// Question 3
+
+// Question 4
+
+// Question 5
+
+// Question 6
+
+// Question 7
+
+// Question 8
+
+// Question 9
+
+// Question 10
