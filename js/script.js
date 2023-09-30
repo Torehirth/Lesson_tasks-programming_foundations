e711909c3b1b51652bb3cdbbb0f531bd07362a6f
Tore Mæland Hirth <torehirth@gmail.com>
2023-09-30 15:45:37 +0200

    add all answers to questions in module 1 - part 1: Strings, numbers and boleans

--- js/script.js
+++ js/script.js
@@ -1,25 +1,92 @@
-console.log("Lesson tasks");
+// Lesson tasks module 1
+
+console.log("Lesson tasks module 1");
+
+// Part: Strings, numbers and Boleans
 
 //Question 1
 var number1 = parseInt(6);
 var number2 = parseFloat(4.5);
 var totalnumber = number1 + number2;
+
 console.log(totalnumber);
+console.log(typeof totalnumber);
 
 // Question 2
+var workingDays = "monday-tuesday-wednesday-thursday-friday";
+
+console.log(workingDays);
+console.log(typeof workingDays);
 
 // Question 3
+var addition = 5 + parseFloat(4.5);
+var subtraction = 5 - parseFloat(4.5);
+var multiplication = 5 * parseFloat(4.5);
+var division = 5 / parseFloat(4.5);
+
+console.log(addition);
+console.log(typeof addition);
+
+console.log(subtraction);
+console.log(typeof subtraction);
+
+console.log(multiplication);
+console.log(typeof multiplication);
+
+console.log(division);
+console.log(typeof division);
 
 // Question 4
+// var firstName = prompt("Name");
+
+// if (firstName == "Santos") {
+//   alert("Santos is true");
+//   console.log("Santos is true");
+// } else {
+//   alert("name is false");
+//   console.log("name is false");
+// }
+
+var isWorking = false;
+
+console.log(typeof isWorking);
 
 // Question 5
+var sentence = "The frogs are angry.";
+
+console.log(sentence.length);
+console.log(typeof sentence);
 
 // Question 6
+var timeElapsed;
+var totalPrice;
 
 // Question 7
+var number1 = 53;
+var number2 = parseFloat(44.6);
 
 // Question 8
+var pet = "donkey";
+console.log(typeof pet);
+
+var age = 89;
+console.log(typeof age);
+
+var discountApplied = true;
+console.log(typeof discountApplied);
+
+var timeLeft;
+console.log(typeof timeLeft);
 
 // Question 9
+// var h1_element = document.querySelector("#h1");
+// h1_element.innerHTML = "Updating heading";
 
 // Question 10
+// var bodyColor = document.querySelector("#body");
+// bodyColor.style.backgroundColor = "lightskyblue";
+// h1_element.style.color = "black";
+
+// Part: Making decisions
+
+// Part: Loops
