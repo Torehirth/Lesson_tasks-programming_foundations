bbc031e0a09d5c8d5757cfcc75d148ae63e46655
Tore Mæland Hirth <torehirth@gmail.com>
2023-10-04 09:54:27 +0200

    did all questions part 2: Arrays in module 2

--- js/script.js
+++ js/script.js
@@ -81,24 +81,88 @@ if (invoicePaid === false || productDispatched === false || customerHasSigned ==
   console.log("Order complete");
 }
 
-// Part: Arrays
+// Part 2: Arrays
 console.log("Part 2: Arrays");
 
+// Question 1: Console log the 3rd item in this array.
+
+var winningNumbers = [1, 76, 2, 78, 16, 7];
+console.log(winningNumbers[2]);
+
+// Question 2: Add a new item to this array using the push method and console log the new amount of items.
+
+var vegetables = ["rice", "potatoes", "porridge", "cake", "spinach"];
+
+vegetables.push("cheese doodles");
+
+console.log(vegetables.length);
+
+// Question 3: Console log each item in this array using a for loop.
+
+var randomThings = ["pumpkin", false, 23, true, "tomato"];
+
+for (var i = 0; i < randomThings.length; i++) {
+  var eachThing = randomThings[i];
+  console.log(eachThing);
+}
+// Question 4: Loop through this array but only log the item if it is a number value.
+
+var moreThings = [14, "cat", true, 29, "fox", true, 34, 19];
+
+// Both answers is correct.
+
+// var numberValues = moreThings.filter(function (item) {
+//   return typeof item === "number";
+// });
+// console.log(numberValues);
+
+for (var i = 0; i < moreThings.length; i++) {
+  var numberValues2 = typeof moreThings[i];
+  if (numberValues2 === "number") {
+    console.log(moreThings[i]);
+  }
+}
+
+// Question 5: Order this array alphabetically and then loop through it and log each item.
+
+var ingredients = ["water", "flour", "toothpaste", "fish lips"];
+
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients.sort()[i]);
+}
+
+// Question 6: Loop through this array and log every second item.
+
+var characters = ["Gob", "Michael", "George Michael", "Lindsay", "Buster", "Maeby", "Tobias", "Lucille"];
+
+// Both answers is correct.
+
+// for (var i = 0; i < characters.length; i += 2) {
+//   console.log(characters[i]);
+// }
+
+for (var i = 0; i < characters.length; i++) {
+  if (i % 2 === 1) {
+    console.log(characters[i]);
+  }
+}
+
+// Part 3: Null, Objects and arrays of objects
+console.log("Part 3: Null, Objects and arrays of objects");
+
+// Question 1:
+// Question 2:
+// Question 3:
+// Question 4:
+// Question 5:
+// Question 6:
+
+// Part 4: Introduction to functions
+console.log("Part 4: Introduction to functions");
+
 // Question 1
 // Question 2
 // Question 3
 // Question 4
-
-// Part: Null, Objects and arrays of objects
-
-// Question 1
-// Question 2
-// Question 3
-// Question 4
-
-// Part: Introduction to functions
-
-// Question 1
-// Question 2
-// Question 3
-// Question 4
+// Question 5:
+// Question 6:
