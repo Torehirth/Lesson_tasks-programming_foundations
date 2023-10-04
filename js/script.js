8fa6aecc270dadcd5bdd300d6e290087d78f618a
Tore Mæland Hirth <torehirth@gmail.com>
2023-10-04 21:29:59 +0200

    did all questions part 3: Null, Objects and arrays of objects - module 2

--- js/script.js
+++ js/script.js
@@ -150,12 +150,119 @@ for (var i = 0; i < characters.length; i++) {
 // Part 3: Null, Objects and arrays of objects
 console.log("Part 3: Null, Objects and arrays of objects");
 
-// Question 1:
-// Question 2:
-// Question 3:
-// Question 4:
-// Question 5:
-// Question 6:
+// Question 1: Console log the price property from the object below:
+
+var product = {
+  name: "Chicken feet",
+  price: 149.99,
+};
+
+console.log(product.price);
+
+// Question 2: Loop through the array below and console log each colour property.
+
+var animals = [
+  {
+    type: "cat",
+    colour: "black",
+  },
+  {
+    type: "elephant",
+    colour: "grey",
+  },
+  {
+    type: "dog",
+    colour: "brown",
+  },
+  {
+    type: "zebra",
+    colour: "black and white",
+  },
+];
+
+for (i = 0; i < animals.length; i++) {
+  console.log(animals[i].colour);
+}
+
+// Question 3: Loop through the array below and console log each name property only if it is on special.
+
+var dinnerOptions = [
+  { name: "Fish and Dips", price: 11.95, onSpecial: true },
+  { name: "Bacon and Frogs", price: 12.99, onSpecial: false },
+  { name: "Mince Flies", price: 19.0, onSpecial: true },
+];
+
+for (i = 0; i < dinnerOptions.length; i++) {
+  if (dinnerOptions[i].onSpecial === true) {
+    console.log(dinnerOptions[i].name);
+  }
+}
+
+// Question 4: Loop through the array below and log each person's name and surname if they are between the ages of 20 and 50 (including 20 and 50):
+
+var people = [
+  {
+    firstName: "Burt",
+    lastName: "Smacharach",
+    age: 35,
+  },
+  {
+    firstName: "Elvis",
+    lastName: "Deadly",
+    age: 55,
+  },
+  {
+    firstName: "Susan",
+    lastName: "Sarongson",
+    age: 42,
+  },
+  {
+    firstName: "Big",
+    lastName: "Gabe",
+    age: 20,
+  },
+];
+
+for (i = 0; i < people.length; i++) {
+  if (people[i].age >= 20 && people[i].age <= 50) {
+    console.log(people[i].firstName + " " + people[i].lastName);
+  }
+}
+
+/* 
+// this is also correct.
+for (var i = 0; i < people.length; i++) {
+  var age = people[i].age;
+  var fullName = people[i].firstName + " " + people[i].lastName;
+
+  if (age >= 20 && age <= 50) {
+    console.log(fullName);
+  }
+}
+*/
+
+// Question 5: Select the ul element in the HTML file, loop through the array below and create a li element containing the product name and price properties.
+
+var products = [
+  {
+    name: "Elephant eggs",
+    price: 45.0,
+  },
+  {
+    name: "Bucket of water",
+    price: 459.99,
+  },
+  {
+    name: "4 cabbages",
+    price: 500.95,
+  },
+];
+
+var listItems = document.querySelector("#list");
+
+for (i = 0; i < products.length; i++) {
+  listItems.innerHTML = listItems.innerHTML + "<li>" + products[i].name + " - " + products[i].price + "</li>";
+}
 
 // Part 4: Introduction to functions
 console.log("Part 4: Introduction to functions");
